432cc90fb4b7adc8eb06caf939ab265f55ae81c1
Guillaume BERNON <guillaumebernonpro@gmail.com>
2024-10-30 10:20:28 +0000

    operational margin v2

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,7 +1,16 @@
 SELECT
     orders_id,
     date_date,
-    ROUND((sm.margin+sh.shipping_fee-sh.logcost-sh.ship_cost),2) AS operational_margin
+    ROUND(SUM((sm.margin+sh.shipping_fee-sh.logcost-sh.ship_cost)),2) AS operational_margin,
+    COUNT(DISTINCT(products_id)) AS nb_product,
+    ROUND(SUM(revenue),2) AS revenue,
+    SUM(quantity) AS quantity,
+    ROUND(SUM(purchase_price),2) AS purchase,
+    ROUND(SUM(purchase_cost),2) AS purchase_cost,
+    ROUND(SUM(shipping_fee),2) AS shipping_fee,
+    ROUND(SUM(logcost),2) AS logcost,
+    SUM(ship_cost) AS ship_cost
 FROM {{ ref('int_sales_margin') }} AS sm
 LEFT JOIN {{ ref('stg_raw__ship') }} AS sh
 USING (orders_id)
+GROUP BY orders_id, date_date
