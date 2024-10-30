5918cde01a04153931e130b403567415b45b9b0d
Guillaume BERNON <guillaumebernonpro@gmail.com>
2024-10-30 14:04:59 +0000

    finance days v2

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -12,6 +12,7 @@
 -- FROM {{ ref('int_orders_operational') }}
 -- GROUP BY date_date ORDER BY date_date DESC
 
+{{ config(materialized=finance_days) }}
 SELECT
      date_date
      ,COUNT(orders_id) AS nb_transactions
